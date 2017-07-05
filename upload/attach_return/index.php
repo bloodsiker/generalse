bb633c79713c32b5c1ee467f253276243a7671ac
bloodsiker <maldini2@ukr.net>
2017-07-05 11:34:47 +0000

    rm upload

--- upload/attach_return/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Lenovo
- * Date: 14.02.2017
- * Time: 15:27
- */
--- upload/import_kpi/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Lenovo
- * Date: 03.03.2017
- * Time: 10:25
- */
