2411512b91a01c962b9e913120961c2ca05cc765
bloodsiker <maldini2@ukr.net>
2017-07-06 09:15:29 +0000

    fix order export excel

--- views/admin/crm/export/orders.php
+++ views/admin/crm/export/orders.php
@@ -47,6 +47,7 @@
             <th style="text-align: center;">Quantity</th>
             <th style="text-align: center;">Price</th>
             <th style="text-align: center;">Type</th>
+            <th style="text-align: center;">Note</th>
             <th style="text-align: center;">Status</th>
             <th style="text-align: center;">Date</th>
             <th style="text-align: center;">Date request</th>
@@ -65,6 +66,7 @@
                     <td style="text-align: center;"><?=$export['quantity']?></td>
                     <td style="text-align: center;"><?=round($export['price'], 2)?></td>
                     <td style="text-align: center;"><?=iconv('WINDOWS-1251', 'UTF-8',$export['type_name'])?></td>
+                    <td style="text-align: center;"><?=iconv('WINDOWS-1251', 'UTF-8',$export['note1'])?></td>
                     <?php $status_name = iconv('WINDOWS-1251', 'UTF-8', $export['status_name'])?>
                     <td style="text-align: center;"><?= $status_name?></td>
                     <td style="text-align: center;">
